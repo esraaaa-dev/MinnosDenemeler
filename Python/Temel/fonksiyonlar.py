2159ab7323319fdefbf70e4e2f0dae33b0822e80
esra nur <eari19@posta.pau.edu.tr>
2023-03-12 17:03:35 +0300

    .

--- Python/Temel/fonksiyonlar.py
+++ Python/Temel/fonksiyonlar.py
@@ -13,7 +13,18 @@ selamla() #parametre verilmezse default parametreyi kullanır
 #her iki fonksiyonun da isimleri aynı fakat birisi parametre alıyor diğeri almıyor 
 #bkz. metotoverloading benzeri
 
+#return eden fonksiyonlar
+def ucgenAlaniHesapla(taban,yukseklik):
+    return taban*yukseklik/2
+alan=ucgenAlaniHesapla(9, 12)
+print("alan= ",alan)
 
+#lambda fonksiyonu
+# fonksiyonun_adi =lambda x1,x2 : x1*x2/2
+ucgenAlaniHesapla2=lambda taban,yukseklik : taban*yukseklik/2
+print("lambda fonksiyonu ile alan :",ucgenAlaniHesapla2(9, 12))
+
+#fibonacci
 def fib(x):
     a=0
     b=1
